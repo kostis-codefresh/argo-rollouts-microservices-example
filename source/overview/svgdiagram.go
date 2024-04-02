e898c456f2412080e37c119e00b50348782bf4ed
Kostis Kapelonis <kostis@codefresh.io>
2024-04-02 17:00:22 +0300

    stable drawing

--- source/overview/svgdiagram.go
+++ source/overview/svgdiagram.go
@@ -10,7 +10,22 @@ func (microserviceStatus *MicroserviceStatus) renderLiveDiagram(w http.ResponseW
 	w.Header().Set("Content-Type", "image/svg+xml")
 	s := svg.New(w)
 	s.Start(1200, 800)
-	s.Circle(250, 250, 125, `fill:none;stroke:black`, `id="circle"`)
+	s.Circle(600, 600, 125, `fill:none;stroke:black`, `id="circle"`)
 	s.Animate("#circle", "opacity", 0, 1, 3, 15)
+
+	s.Rect(10, 200, 200, 100, `fill:lightblue;stroke:black`, `id="frontendActive"`)
+
+	s.Rect(300, 200, 200, 100, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+
+	s.Rect(500, 30, 200, 100, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+
+	s.Ellipse(900, 220, 50, 20, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	s.Ellipse(900, 200, 50, 20, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	s.Ellipse(900, 180, 50, 20, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	s.Ellipse(900, 160, 50, 20, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	s.Ellipse(900, 140, 50, 20, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	s.Ellipse(900, 120, 50, 20, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	s.Ellipse(900, 100, 50, 20, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+
 	s.End()
 }
