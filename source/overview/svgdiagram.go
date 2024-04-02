34ad0039c442b70322af54566fe311964ad99692
Kostis Kapelonis <kostis@codefresh.io>
2024-04-02 18:11:25 +0300

    some constants for rendering the SVG

--- source/overview/svgdiagram.go
+++ source/overview/svgdiagram.go
@@ -6,26 +6,41 @@ import (
 	svg "github.com/ajstarks/svgo"
 )
 
+const (
+	sceneWidth  = 1200
+	sceneHeight = 800
+
+	rolloutWidth  = 200
+	rolloutHeight = 100
+
+	queueDiskHeight = 20
+	queueDiskWidth  = 50
+)
+
 func (microserviceStatus *MicroserviceStatus) renderLiveDiagram(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "image/svg+xml")
 	s := svg.New(w)
-	s.Start(1200, 800)
+	s.Start(sceneWidth, sceneHeight)
 	s.Circle(600, 600, 125, `fill:none;stroke:black`, `id="circle"`)
 	s.Animate("#circle", "opacity", 0, 1, 3, 15)
 
-	s.Rect(10, 200, 200, 100, `fill:lightblue;stroke:black`, `id="frontendActive"`)
+	s.Rect(10, 200, rolloutWidth, rolloutHeight, `fill:lightblue;stroke:black`, `id="frontendActive"`)
 
-	s.Rect(300, 200, 200, 100, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	s.Rect(300, 200, rolloutWidth, rolloutHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
 
-	s.Rect(500, 30, 200, 100, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	s.Rect(500, 30, rolloutWidth, rolloutHeight, `fill:lightblue;stroke:black`, `id="workerActive"`)
 
-	s.Ellipse(900, 220, 50, 20, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
-	s.Ellipse(900, 200, 50, 20, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
-	s.Ellipse(900, 180, 50, 20, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
-	s.Ellipse(900, 160, 50, 20, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
-	s.Ellipse(900, 140, 50, 20, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
-	s.Ellipse(900, 120, 50, 20, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
-	s.Ellipse(900, 100, 50, 20, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	s.Ellipse(900, 220, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	s.Ellipse(900, 200, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	s.Ellipse(900, 180, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	s.Ellipse(900, 160, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	s.Ellipse(900, 140, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	s.Ellipse(900, 120, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	s.Ellipse(900, 100, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+
+	s.Line(250, 240, 280, 250, `stroke-width:3;stroke:black`)
+	s.Line(220, 250, 280, 250, `stroke-width:3;stroke:black`)
+	s.Line(250, 260, 280, 250, `stroke-width:3;stroke:black`)
 
 	s.End()
 }
