0112358a3589444d9894ae494fd395ab20ad55f9
Kostis Kapelonis <kostis@codefresh.io>
2024-04-02 19:09:07 +0300

    queue has its own drawing function

--- source/overview/svgdiagram.go
+++ source/overview/svgdiagram.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	svg "github.com/ajstarks/svgo"
@@ -15,7 +16,18 @@ const (
 
 	queueDiskHeight = 20
 	queueDiskWidth  = 50
+
+	numberOfDisks = 7
+	diskSpacing   = 20
+
+	margin = 10
 )
+
+type queueGFX struct {
+	color       string
+	name        string
+	description string
+}
 
 func (microserviceStatus *MicroserviceStatus) renderLiveDiagram(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "image/svg+xml")
@@ -30,17 +42,49 @@ func (microserviceStatus *MicroserviceStatus) renderLiveDiagram(w http.ResponseW
 
 	s.Rect(500, 30, rolloutWidth, rolloutHeight, `fill:lightblue;stroke:black`, `id="workerActive"`)
 
-	s.Ellipse(900, 220, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
-	s.Ellipse(900, 200, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
-	s.Ellipse(900, 180, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
-	s.Ellipse(900, 160, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
-	s.Ellipse(900, 140, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
-	s.Ellipse(900, 120, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
-	s.Ellipse(900, 100, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	// s.Ellipse(900, 220, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	// s.Ellipse(900, 200, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	// s.Ellipse(900, 180, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	// s.Ellipse(900, 160, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	// s.Ellipse(900, 140, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	// s.Ellipse(900, 120, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+	// s.Ellipse(900, 100, queueDiskWidth, queueDiskHeight, `fill:lightblue;stroke:black`, `id="frontendPreview"`)
+
+	q := queueGFX{}
+	q.color = "orange"
+	q.name = "production"
+	q.description = "RabbitMQ"
+	microserviceStatus.renderQueue(s, q)
 
 	s.Line(250, 240, 280, 250, `stroke-width:3;stroke:black`)
 	s.Line(220, 250, 280, 250, `stroke-width:3;stroke:black`)
 	s.Line(250, 260, 280, 250, `stroke-width:3;stroke:black`)
 
+	s.Text(10, 200, "Front-end", "font-size:30px;fill:black")
+	s.Text(300, 200, "Back-end", "font-size:30px;fill:black")
+	s.Text(500, 30, "Worker", "font-size:30px;fill:black")
+
 	s.End()
+}
+
+func (microserviceStatus *MicroserviceStatus) renderQueue(canvas *svg.SVG, queue queueGFX) {
+	x := 900
+	y := 100
+
+	fontSize := 26
+	svgTextOptions := fmt.Sprintf("font-size:%dpx;fill:black", fontSize)
+	svgDiskOptions := fmt.Sprintf("fill:%s;stroke:black", queue.color)
+
+	startingY := y + (diskSpacing * numberOfDisks)
+	for i := 0; i < numberOfDisks; i++ {
+		canvas.Ellipse(x, startingY, queueDiskWidth, queueDiskHeight, svgDiskOptions, `id="frontendPreview"`)
+		startingY = startingY - diskSpacing
+	}
+
+	//Description on top of the disks
+	canvas.Text(x-queueDiskWidth, y-margin, queue.description, svgTextOptions)
+
+	//queue name is to the right of the disks
+	canvas.Text(x+queueDiskWidth+margin, y+(diskSpacing*numberOfDisks/2), queue.name, svgTextOptions)
+
 }
